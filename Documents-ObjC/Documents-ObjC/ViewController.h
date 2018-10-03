947ea643b7f4b1bb147c5299b682636222d5bfcd
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-03 12:30:40 -0700

    blank project with prefixing

---
+++ Documents-ObjC/Documents-ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Documents-ObjC
+//
+//  Created by Jonathan T. Miles on 10/3/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Documents-ObjC/Documents-ObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Documents-ObjC
+//
+//  Created by Jonathan T. Miles on 10/3/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
